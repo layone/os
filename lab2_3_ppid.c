098cce7d0ec1ab20253e97a25cd4a509dd6b4195
layone <32382235+layone@users.noreply.github.com>
2019-03-20 19:51:10 +0800

    Create lab2_3_ppid.c

---
+++ lab2_3_ppid.c
@@ -0,0 +1,54 @@
+ #include "stdio.h" 
+#include "sys/types.h" 
+#include "unistd.h"
+#include "stdlib.h" 
+#define HASPRO -10 
+int main() 
+{ 
+pid_t p1,p2,p3,p4; 
+int cnt=0; 
+while((p1=fork()) == -1);
+if(!p1) 
+{ 
+while((p2=fork()) == -1); 
+if(!p2) 
+{ 
+while((p3=fork())==-1); 
+if (!p3) 
+{ 
+{
+printf("Node p3 is p2's child with pid %d,it's parent pid %d.\n",getpid(),getppid()); 
+
+} 
+} else 
+{ 
+while ((p4=fork())==-1); 
+if (!p4) 
+{ 
+{ 
+printf("Node p4 is p2's child with pid %d, it's parent pid %d.\n",getpid(),getppid()); 
+} 
+} 
+else
+{ 
+; 
+} 
+} 
+{ 
+printf("Node p2 is p1's child with pid %d, it's parent pid %d.\n",getpid(),getppid()); 
+} 
+} 
+else 
+{
+ ; 
+}
+{ 
+printf("Node p1 is parent with pid %d, it's parent pid %d.\n",getpid(),getppid()); 
+} 
+} 
+else 
+{
+ ;
+} 
+return 0; 
+} 
